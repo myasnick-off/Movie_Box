7b2669acdcb6089707d7d981a30ede47c380fea0
myasnikov.d <d.myasnikov@teledoc.ru>
2022-09-06 10:48:00 +0300

    закоммитил файл BuildConfig

---
+++ app/build/generated/source/buildConfig/debug/com/example/moviebox/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.moviebox;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.moviebox";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0-debug";
+  // Field from build type: debug
+  public static final String API_KEY = "02aa4f3b7db68ceb4c5c389d5dee7ce4";
+  // Field from build type: debug
+  public static final String TYPE = "DEBUG";
+}
